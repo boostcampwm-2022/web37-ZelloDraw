7ab4b1f356a676686873240f083492df79a9e9a6
LouiMinister <louiminister@gmail.com>
2022-11-10 18:25:27 +0900

    chore: backend eslint, prettier 설정 변경
    eslint, prettier 설정을 변경함
    ## Intent

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
+    'standard-with-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -14,6 +15,8 @@ module.exports = {
   env: {
     node: true,
     jest: true,
+    browser: true,
+    es2021: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -21,5 +24,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
   },
 };
