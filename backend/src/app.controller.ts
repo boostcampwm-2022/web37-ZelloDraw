08b8157e710814512749bbcce1649849139aefed
LouiMinister <louiminister@gmail.com>
2022-11-13 18:11:45 +0900

    chore: ping 라우트 경로 추가
    서버 상태 판별을 위함
    ## Intent

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('ping')
   getHello(): string {
-    return this.appService.getHello();
+    return 'pong';
   }
 }
