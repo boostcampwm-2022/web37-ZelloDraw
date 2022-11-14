22ad75cf46fda73520cdbcfa015f0fd1fdfd7818
LouiMinister <louiminister@gmail.com>
2022-11-14 20:34:24 +0900

    refactor: [BE] LobbyStore에서 관리하는 Lobby의 타입 지정
    - Intent

--- backend/src/core/lobby.service.ts
+++ backend/src/core/lobby.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
 
+export interface LobbyStore {
+    [key: string]: Lobby;
+}
+
+export interface Lobby {
+    id: string;
+    owner: Socket;
+    players: Socket[];
+}
+
 @Injectable()
 export class LobbyService {
-    store = {};
+    store: LobbyStore = {};
 
     createLobby(client: Socket): string {
         const lobbyId = client.id + new Date().toString();
         this.store[lobbyId] = {
             id: lobbyId,
             owner: client,
+            players: [],
         };
         return lobbyId;
     }
