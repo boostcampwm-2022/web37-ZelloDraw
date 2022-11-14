a8bc172778472851bb2401560d0168b8c9f60bcb
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2022-11-14 23:21:36 +0900

    feat: [FE] 배경 추가 및 Card 컴포넌트 수정
    배경 이미지(svg) 추가 및 적용, Card 컴포넌트 border 색상 수정
    
    -- Intent
    border에 linear-gradient 적용 시 배경색에 투명색을 넣을 수 없는 문제점
    발생

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -10,12 +10,6 @@ export default Card;
 const CardDiv = styled.div`
     background: ${({ theme }) => theme.color.whiteT1};
     box-shadow: ${({ theme }) => theme.shadow.card};
+    border: 1px solid ${({ theme }) => theme.color.brown};
     border-radius: 40px;
-    padding: 1px;
-    border: 1px solid solid;
-    background: ${({ theme }) => theme.color.primaryLightBrown};
-    background-image: linear-gradient(#fff, #fff),
-        ${({ theme }) => theme.gradation.primaryLightBrown};
-    background-origin: border-box;
-    background-clip: content-box, border-box;
 `;
