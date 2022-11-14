6cc07584db2bfeca87c4da37e2a0f08683824f02
jayjeong8 <zuni4242@gmail.com>
2022-11-14 15:38:00 +0900

    [FE] feat: 전역스타일 및 테마 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { ZelloTheme } from './styles/ZelloTheme';
+import { GlobalStyle } from '@styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <ThemeProvider theme={ZelloTheme}>
+                <App />
+                <GlobalStyle />
+            </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>,
 );
