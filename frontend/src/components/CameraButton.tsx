4dec1c6a3de54d98a03944b78e59b7fb37ff0fd0
jayjeong8 <zuni4242@gmail.com>
2022-11-15 01:08:25 +0900

    feat: [FE] Game 페이지 추가

--- frontend/src/components/CameraButton.tsx
+++ frontend/src/components/CameraButton.tsx
@@ -24,7 +24,7 @@ const CameraBtnSet = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: 80px;
+    width: 84px;
 `;
 
 const CameraBtn = styled.button`
