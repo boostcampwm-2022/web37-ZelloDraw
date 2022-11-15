94b2acc05e7ef5d6c4ef3abdc7d1b05587e682ed
LouiMinister <louiminister@gmail.com>
2022-11-15 21:44:04 +0900

    fix: [BE] joinLobby 핸들러에서 return, re-emit 하는 데이터 형식 재정의
    - Intent
    문서와 일치하지 않음.

--- backend/src/core/user.dto.ts
+++ backend/src/core/user.dto.ts
@@ -12,3 +12,9 @@ export class JoinLobbyRequest {
     @IsNotEmpty()
     public lobbyId: string;
 }
+
+export type JoinLobbyResponse = Array<{ userName: string }>;
+
+export interface JoinLobbyReEmitRequest {
+    userName: string;
+}
