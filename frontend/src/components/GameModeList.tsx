5ee5475392ddcf5a69ffd58fa82b7bb8b71d3a37
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2022-11-17 13:11:35 +0900

    feat: [FE] 게스트의 경우 시작 대기 중 메세지 표시
    - Intent

--- frontend/src/components/GameModeList.tsx
+++ frontend/src/components/GameModeList.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
 import styled from 'styled-components';
 import Card from '@components/Card';
 import GameModeItem from '@components/GameModeItem';
 import { ReactComponent as GirlWithPencilChar } from '@assets/girl-with-pencil 1.svg';
 import PrimaryButton from '@components/PrimaryButton';
+import { useRecoilValue } from 'recoil';
+import { userState, userStateType } from '@atoms/user';
 
 function GameModeList() {
-    const [selected, setSelected] = useState<number>(0);
+    const user = useRecoilValue<userStateType>(userState);
 
     const modes = [
         {
@@ -20,11 +21,17 @@ function GameModeList() {
         <Card>
             <CardInner>
                 {modes.map((mode, idx) => (
-                    <GameModeItem mode={mode} key={mode.title} isSelected={selected === idx} />
+                    <GameModeItem mode={mode} key={mode.title} isSelected={idx === 0} />
                 ))}
-                <ButtonWrapper>
-                    <PrimaryButton topText='START GAME' bottomText='시작하기' />
-                </ButtonWrapper>
+                {user.isHost ? (
+                    <ButtonWrapper>
+                        <PrimaryButton topText='START GAME' bottomText='시작하기' />
+                    </ButtonWrapper>
+                ) : (
+                    <TextWrapper>
+                        <span>*</span> 게임 시작을 기다리고 있어요... <span>*</span>
+                    </TextWrapper>
+                )}
             </CardInner>
         </Card>
     );
@@ -41,4 +48,26 @@ const CardInner = styled.div`
 
 const ButtonWrapper = styled.div`
     margin: auto 0 0 auto;
+`;
+
+const TextWrapper = styled.div`
+    font-style: normal;
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.typo.h5};
+    line-height: 26px;
+    text-align: center;
+    letter-spacing: -0.045em;
+    color: ${({ theme }) => theme.color.white};
+    margin: auto;
+    margin-bottom: 16px;
+
+    background: ${({ theme }) => theme.gradation.yellowPurple};
+    -webkit-background-clip: text;
+    -webkit-text-stroke: 1px transparent;
+
+    span {
+        font-family: 'Sniglet';
+        font-weight: 800;
+        letter-spacing: -0.05em;
+    }
 `;
