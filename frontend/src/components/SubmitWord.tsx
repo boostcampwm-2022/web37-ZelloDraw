a8bca6a1349564251340598b0bc89e7f3fc49a0e
jayjeong8 <zuni4242@gmail.com>
2022-11-21 21:34:13 +0900

    feat: [FE] 0번 라운드일때 답변인풋 플레이스홀더에서 유저에게 랜덤 단어를 보여주는 기능

--- frontend/src/components/SubmitWord.tsx
+++ frontend/src/components/SubmitWord.tsx
@@ -1,15 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Center } from '@styles/styled';
-import PrimaryButton from '@components/PrimaryButton';
 import { useRecoilValue } from 'recoil';
-import { roundDrawState } from '@atoms/game';
+import { roundDrawState, roundNumberState, roundWordState } from '@atoms/game';
+import PrimaryButton from '@components/PrimaryButton';
 
 export default function SubmitWord() {
     const isDraw = useRecoilValue(roundDrawState);
+    const roundNum = useRecoilValue(roundNumberState);
+    const roundWord = useRecoilValue(roundWordState);
+    const [placeholder, setPlaceholder] = useState('그림을 보고 답을 맞춰보세요!');
+
+    useEffect(() => {
+        setRandomWordToPlaceholder();
+    }, []);
+
+    function setRandomWordToPlaceholder() {
+        // 0번 라운드일때만 인풋 플레이스홀더에서 유저에게 랜덤 단어를 보여준다.
+        if (roundNum === 0 && roundWord !== '') {
+            setPlaceholder(roundWord);
+        }
+    }
 
     return (
         <Container>
-            {!isDraw ? <AnswerInput placeholder={'그림을 보고 답을 맞춰보세요!'} /> : <div />}
+            {!isDraw ? <AnswerInput placeholder={placeholder} /> : <div />}
             <PrimaryButton topText={'SUBMIT'} bottomText={'제출하기'} />
         </Container>
     );
