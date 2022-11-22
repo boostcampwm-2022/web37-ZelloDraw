d030d209e98d128ad6734d7cfe774bc3ed9772ea
JirongKim <munja__@naver.com>
2022-11-22 18:25:31 +0900

    feat: [BE] gameLobby Repository 구현
    - Intent
    Game Service, Lobby Service에서 동일한 GameLobby store를 사용하기 위해 Repository 구현

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -3,8 +3,9 @@ import { CoreGateway } from './core.gateway';
 import { LobbyService } from './lobby.service';
 import { UserService } from './user.service';
 import { RoundService } from './round.service';
+import { GameLobbyRepository } from './gamelobby.repository';
 
 @Module({
-    providers: [CoreGateway, LobbyService, UserService, RoundService],
+    providers: [CoreGateway, LobbyService, UserService, RoundService, GameLobbyRepository],
 })
 export class CoreModule {}
---
+++ backend/src/core/gamelobby.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { GameLobby } from './gameLobby.model';
+import { User } from './user.model';
+
+@Injectable()
+export class GameLobbyRepository {
+    gameLobbyStore: { [key: string]: GameLobby };
+
+    constructor() {
+        this.gameLobbyStore = {};
+    }
+
+    create(user: User): string {
+        const gameLobby = new GameLobby(user);
+        this.gameLobbyStore[gameLobby.getId()] = gameLobby;
+        return gameLobby.getId();
+    }
+
+    findByUser(user: User): GameLobby {
+        return this.gameLobbyStore[user.lobbyId];
+    }
+
+    findById(lobbyId: string): GameLobby {
+        return this.gameLobbyStore[lobbyId];
+    }
+
+    save(gameLobby: GameLobby) {
+        this.gameLobbyStore[gameLobby.getId()] = gameLobby;
+    }
+}
