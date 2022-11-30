d424a0920bd35e5552f9ec3c0cfaf1d59aec18c1
jayjeong8 <zuni4242@gmail.com>
2022-12-01 00:35:57 +0900

    feat: [FE] 방장일 때와 게스트일 때 결과 스케치북 UI가 다르게 나타나는 기능
    - Intent
    방장만 스케치북을 이동할 수 있는 UI가 보여야해서 적용되었어요.

--- frontend/src/components/ResultSketchbook.tsx
+++ frontend/src/components/ResultSketchbook.tsx
@@ -7,20 +7,23 @@ import {
     maxSketchbookState,
     sketchbookAuthorState,
 } from '@atoms/result';
+import { userState } from '@atoms/user';
 import SketchbookCard from '@components/SketchbookCard';
 import RoundNumber from '@components/RoundNumber';
 import ResultGuide from '@components/ResultGuide';
 import QuizResultContent from '@components/QuizResultContent';
 import useCheckGuidePage from '@hooks/useCheckGuidePage';
 import useResultSketchbook from '@hooks/useResultSketchbook';
+import { ReactComponent as LeftArrowIcon } from '@assets/icons/chevron-left-gradient.svg';
+import { ReactComponent as RightArrowIcon } from '@assets/icons/chevron-right-gradient.svg';
 
 function ResultSketchbook() {
     const { maxPageNum } = useRecoilValue(maxSketchbookState);
     const currentSketchbook = useRecoilValue(currentSketchbookState);
     const sketchbookAuthor = useRecoilValue(sketchbookAuthorState);
     const currentPageIdx = useRecoilValue(currentPageIdxState);
+    const { isHost } = useRecoilValue(userState);
     const { checkIsNotGuidePage } = useCheckGuidePage();
-
     useResultSketchbook();
 
     return (
@@ -42,7 +45,15 @@ function ResultSketchbook() {
                 }
             />
             <SketchbookAuthor>
-                {checkIsNotGuidePage() && `${sketchbookAuthor}의 스케치북`}
+                {
+                    <>
+                        {isHost ? <LeftArrowIcon /> : <Brace>&#123;</Brace>}
+                        <AuthorName isHost={isHost}>{sketchbookAuthor}</AuthorName>
+                        <AuthorName isHost={isHost}>유저1355</AuthorName>
+                        {isHost ? <RightArrowIcon /> : <Brace>&#125;</Brace>}
+                        <span>의 스케치북</span>
+                    </>
+                }
             </SketchbookAuthor>
         </>
     );
@@ -63,5 +74,30 @@ const SketchbookAuthor = styled(Center)`
     margin-top: 26px;
     color: ${({ theme }) => theme.color.whiteT2};
     font-size: ${({ theme }) => theme.typo.h2};
-    text-shadow: ${({ theme }) => theme.shadow.text};
+
+    > svg {
+        margin: 0 28px;
+        transform: scale(1.3) translateY(2px);
+        cursor: pointer;
+    }
+    > span {
+        font-weight: 600;
+    }
+`;
+
+const AuthorName = styled.span<{ isHost: boolean | null }>`
+    background: ${(props) =>
+        props.isHost ? props.theme.gradation.yellowPurple : props.theme.color.whiteT2};
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    text-fill-color: transparent;
+`;
+
+const Brace = styled.span`
+    margin: 0 6px;
+    font-family: 'Sniglet', cursive;
+    font-weight: 700;
+    font-size: ${({ theme }) => theme.typo.h1};
+    transform: translateY(4px);
 `;
