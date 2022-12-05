3f794c6ce8394bfd620441aa9ebc6b5a192a3182
jayjeong8 <zuni4242@gmail.com>
2022-12-06 00:21:08 +0900

    feat: [FE] 스케치북이 바뀌면 이동하는 모션

--- frontend/src/components/SketchbookCard.tsx
+++ frontend/src/components/SketchbookCard.tsx
@@ -1,7 +1,11 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Center } from '@styles/styled';
-import { ReactComponent as SketchbookImg } from '@assets/sketchbook.svg';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useRecoilValue } from 'recoil';
+import { currentBookIdxState } from '@atoms/result';
+import { CANVAS_WIDTH } from '@utils/constants';
+import SketchbookImg from '@assets/sketchbook.svg';
 import Card from '@components/Card';
 
 interface SketchbookCardType {
@@ -10,21 +14,70 @@ interface SketchbookCardType {
     right?: ReactNode;
 }
 
+const slideVariants = {
+    enter: ({ direction, xValue }: { direction: number; xValue: number }) => {
+        return {
+            x: direction * xValue,
+            opacity: 0,
+        };
+    },
+    center: {
+        zIndex: 1,
+        x: 0,
+        opacity: 1,
+    },
+    exit: ({ direction, xValue }: { direction: number; xValue: number }) => {
+        return {
+            zIndex: 0,
+            x: direction * xValue * -1,
+            opacity: 0,
+        };
+    },
+};
+
 function SketchbookCard({ left, center, right }: SketchbookCardType) {
+    const currentBookIdx = useRecoilValue(currentBookIdxState);
+    const [lastBookIdx, setLastBookIdx] = useState(0);
+    const [direction, setDirection] = useState(1);
+    const xValue = 600;
+
+    useEffect(() => {
+        if (currentBookIdx > lastBookIdx) {
+            setDirection(1);
+        } else {
+            setDirection(-1);
+        }
+        setLastBookIdx(currentBookIdx);
+    }, [currentBookIdx]);
+
     return (
         <Card>
             <Container>
                 <LeftSide>{left}</LeftSide>
                 <SketchbookWrapper>
-                    <SketchbookImg />
-                    {center}
+                    <AnimatePresence initial={false} custom={{ direction, xValue }}>
+                        <motion.div
+                            key={currentBookIdx}
+                            custom={{ direction, xValue }}
+                            variants={slideVariants}
+                            initial='enter'
+                            animate='center'
+                            exit='exit'
+                            transition={{
+                                x: { type: 'spring', damping: 30, stiffness: 300 },
+                                opacity: { duration: 0.2 },
+                            }}
+                        >
+                            <img src={SketchbookImg} alt={'sketchbook'} />
+                            {center}
+                        </motion.div>
+                    </AnimatePresence>
                 </SketchbookWrapper>
                 <RightSide>{right}</RightSide>
             </Container>
         </Card>
     );
 }
-
 export default SketchbookCard;
 
 const Container = styled(Center)`
@@ -39,7 +92,12 @@ const LeftSide = styled.div`
 
 const SketchbookWrapper = styled.div`
     position: relative;
+    width: ${CANVAS_WIDTH}px;
+    height: 560px;
     margin: 0 30px;
+    > div {
+        position: absolute;
+    }
 `;
 
 const RightSide = styled.div`
