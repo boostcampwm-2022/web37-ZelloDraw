6002d9aa740f487459e1e381a5de5346c71ad43f
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2022-12-06 15:43:26 +0900

    feat: [FE] 유저의 URL 직접 접근 차단
    - url 직접 접근을 막기 위해 ProtectRoute 추가
    - 정해진 url path 외의 경로에 접근 시 메인페이지로 리다이렉션 시킴
    
    - Intent
    유저가 메인 페이지를 거치지 않고 로비나 게임페이지로 직접 접근하는 예외
    상황을 방지하는 목적으로 추가했어요.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import Main from '@pages/Main';
 import Lobby from '@pages/Lobby';
 import Game from '@pages/Game';
+import ProtectRoute from '@components/route/ProtectRoute';
 
 function App() {
     return (
         <RecoilRoot>
             <Routes>
                 <Route path='/' element={<Main />} />
-                <Route path='/lobby' element={<Lobby />} />
-                <Route path='/game' element={<Game />} />
+                <Route
+                    path='/lobby'
+                    element={
+                        <ProtectRoute>
+                            <Lobby />
+                        </ProtectRoute>
+                    }
+                />
+                <Route
+                    path='/game'
+                    element={
+                        <ProtectRoute>
+                            <Game />
+                        </ProtectRoute>
+                    }
+                />
+                <Route path='/*' element={<Navigate to='/' replace={true} />} />
             </Routes>
         </RecoilRoot>
     );
