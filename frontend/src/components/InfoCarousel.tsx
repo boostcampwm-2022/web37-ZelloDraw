50c9bb9e3b5b947175d4397207b637e5631a04c0
jayjeong8 <zuni4242@gmail.com>
2022-12-06 17:46:10 +0900

    refactor: [FE] 메인 설명 슬라이드를 framer-motion으로 대체
    - Intent
    스케치북이동에서 만들었던 framer-motion Variants를 재사용하고, 기존 margin으로 동작하던 애니메이션을 transform으로 변경하여 성능을 3배 정도 개선했어요.

--- frontend/src/components/InfoCarousel.tsx
+++ frontend/src/components/InfoCarousel.tsx
@@ -1,56 +1,64 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as LeftArrowIcon } from '@assets/icons/chevron-left-icon.svg';
 import { ReactComponent as RightArrowIcon } from '@assets/icons/chevron-right-icon.svg';
+import { motion, AnimatePresence } from 'framer-motion';
+import { slideVariants } from '@utils/framerMotion';
 
 function InfoCarousel() {
     const [current, setCurrent] = useState<number>(0);
-    const [style, setStyle] = useState({
-        marginLeft: `-${current}00%`,
-    });
+    const [direction, setDirection] = useState(1);
     const carouselSize = useRef(4);
+    const xValue = 439;
+
     const contents: React.ReactNode[] = [
-        <SlideInner>
+        <>
             <h3>즐거운 시간 보낼 준비가 되었나요?</h3>
             <h3>처음이라면 옆으로 넘겨</h3>
             <h3>도움말을 읽어보세요.</h3>
-        </SlideInner>,
-        <SlideInner>
+        </>,
+        <>
             <h3>게임이 시작되면 각 플레이어는</h3>
             <h3>랜덤 제시어를 확인할 수 있어요.</h3>
-        </SlideInner>,
-        <SlideInner>
+        </>,
+        <>
             <h3>다른 사람의 제시어를 확인하고</h3>
             <h3>제시어를 잘 묘사할 수 있는</h3>
             <h3>독특한 그림을 그려보아요!</h3>
-        </SlideInner>,
-        <SlideInner>
+        </>,
+        <>
             <h3>다른 사람의 그림을 보고</h3>
             <h3>답을 유추해보기도 해요!</h3>
-        </SlideInner>,
+        </>,
     ];
 
     const moveSlide = (i: number) => {
         let nextIndex = current + i;
         if (nextIndex < 0) nextIndex = carouselSize.current - 1;
         else if (nextIndex >= carouselSize.current) nextIndex = 0;
+
+        setDirection(i * -1);
         setCurrent(nextIndex);
     };
-
-    useEffect(() => {
-        setStyle({ marginLeft: `-${current}00%` });
-    }, [current]);
 
     return (
         <CarouselContainer>
             <Slide>
                 <LeftArrowIcon className={'arrowIcon'} onClick={() => moveSlide(-1)} />
                 <Window>
-                    <FlexBox style={style}>
-                        {contents.map((elements, i) => (
-                            <div key={i}>{elements}</div>
-                        ))}
-                    </FlexBox>
+                    <AnimatePresence initial={false} custom={{ direction, xValue }}>
+                        <SlideInner
+                            key={current}
+                            custom={{ direction, xValue }}
+                            variants={slideVariants}
+                            initial='enter'
+                            animate='center'
+                            exit='exit'
+                            transition={{ duration: 0.3, ease: 'easeOut' }}
+                        >
+                            {contents[current]}
+                        </SlideInner>
+                    </AnimatePresence>
                 </Window>
                 <RightArrowIcon className={'arrowIcon'} onClick={() => moveSlide(1)} />
             </Slide>
@@ -75,29 +83,25 @@ const CarouselContainer = styled.div`
         margin: 24px;
         cursor: pointer;
     }
-
-    div {
-        transition: all 0.3s ease-out;
-    }
 `;
 
 const Slide = styled.div`
     display: flex;
 `;
 
-const SlideInner = styled.div`
+const SlideInner = styled(motion.div)`
     width: 439px;
     height: 144px;
+    position: absolute;
+    top: 0;
+    left: 0;
 `;
 
 const Window = styled.div`
     overflow: hidden;
     width: 439px;
     height: 144px;
-`;
-
-const FlexBox = styled.div`
-    display: flex;
+    position: relative;
 `;
 
 const DotButtonSet = styled.div`
