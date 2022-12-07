1cce4b7e2795a066917278130201dd35eacf17ad
JirongKim <munja__@naver.com>
2022-12-07 17:12:46 +0900

    refactor: [BE] Game DTO 클래스로 일괄 수정 -Intent Interface -> Class로 코드 수정하였습니다.

--- backend/src/core/game.dto.ts
+++ backend/src/core/game.dto.ts
@@ -2,36 +2,71 @@ import { QuizReply, QuizReplyType } from './quizReply.model';
 import { PartialWithoutMethods } from '../utils/types';
 
 // TODO: user 정보에서 socketId 정보는 제외하자.
-export interface QuizReplyRequest {
+export class QuizReplyRequest {
     type: QuizReplyType;
     content: string;
 }
 
-export interface StartRoundEmitRequest {
+export class StartRoundEmitRequest {
     roundType: QuizReplyType;
     quizReply: PartialWithoutMethods<QuizReply>;
     curRound: number;
     maxRound: number;
     limitTime: number;
+
+    constructor(
+        roundType: QuizReplyType,
+        quizReply: PartialWithoutMethods<QuizReply>,
+        curRound: number,
+        maxRound: number,
+        limitTime: number,
+    ) {
+        this.roundType = roundType;
+        this.quizReply = quizReply;
+        this.curRound = curRound;
+        this.maxRound = maxRound;
+        this.limitTime = limitTime;
+    }
 }
 
-export interface SubmitQuizReplyRequest {
+export class SubmitQuizReplyRequest {
     quizReply: QuizReplyRequest;
+
+    constructor(quizReply: QuizReplyRequest) {
+        this.quizReply = quizReply;
+    }
 }
 
-export interface SubmitQuizReplyEmitRequest {
+export class SubmitQuizReplyEmitRequest {
     submittedQuizReplyCount: number;
+
+    constructor(submittedQuizReplyCount: number) {
+        this.submittedQuizReplyCount = submittedQuizReplyCount;
+    }
 }
 
-export interface CompleteGameEmitRequest {
+export class CompleteGameEmitRequest {
     quizReplyLists: Array<Array<PartialWithoutMethods<QuizReply>>>;
+
+    constructor(quizReplyLists: Array<Array<PartialWithoutMethods<QuizReply>>>) {
+        this.quizReplyLists = quizReplyLists;
+    }
 }
 
-export interface WatchResultSketchbookRequest {
+export class WatchResultSketchbookRequest {
     bookIdx: number;
+
+    constructor(bookIdx: number) {
+        this.bookIdx = bookIdx;
+    }
 }
 
-export interface WatchResultSketchbookEmitRequest {
+export class WatchResultSketchbookEmitRequest {
     bookIdx: number;
     isWatched: boolean;
+
+    constructor(bookIdx: number, isWatched: boolean) {
+        this.bookIdx = bookIdx;
+        this.isWatched = isWatched;
+    }
 }
