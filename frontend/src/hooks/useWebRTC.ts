b0eb8367c9a81e4700662adf0af10c6dffb4a372
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2022-12-12 17:43:15 +0900

    fix: [FE] stream delete 로직 위치 수정
    - Intent

--- frontend/src/hooks/useWebRTC.ts
+++ frontend/src/hooks/useWebRTC.ts
@@ -116,6 +116,12 @@ function useWebRTC() {
             NetworkService.off('webrtc-offer');
             NetworkService.off('webrtc-answer');
             NetworkService.off('webrtc-ice');
+        };
+    }, [streamMap]);
+
+    useEffect(() => {
+        return () => {
+            console.log('done');
             streamMap.forEach((stream, sid) => {
                 if (!stream || !pcsRef.current[sid]) return;
                 pcsRef.current[sid].close();
@@ -123,7 +129,7 @@ function useWebRTC() {
                 delete pcsRef.current[sid];
             });
         };
-    }, [streamMap]);
+    }, []);
 
     return { createOffers };
 }
