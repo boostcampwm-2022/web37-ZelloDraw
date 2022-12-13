7ffdd488724a98e468916d62220d0e3d40f0eb86
JirongKim <munja__@naver.com>
2022-12-13 17:10:41 +0900

    feat: [BE] Nest.js config 추가 - Intent Notion API Secret과 DB ID를 저장하기 위해 .env 파일을 사용하기 위함이에요. Backend 폴더에 .env 파일 추가가 필요해요

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { RedisModule } from './redis/redis.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [CoreModule, RedisModule, MongooseModule.forRoot('mongodb://localhost/zellodraw')],
+    imports: [
+        CoreModule,
+        RedisModule,
+        MongooseModule.forRoot('mongodb://localhost/zellodraw'),
+        ConfigModule.forRoot(),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
